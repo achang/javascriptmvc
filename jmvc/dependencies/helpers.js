a9bc2cc4d3773c98f4e7cace2652134cb4bad8e3
JustinBMeyer <JustinBMeyer@f352ecdf-183f-0410-ae60-f1561b901441>
2008-02-02 05:06:00 +0000

    cleaned for packer
    git-svn-id: http://javascriptmvc.googlecode.com/svn/trunk@74 f352ecdf-183f-0410-ae60-f1561b901441

--- jmvc/dependencies/helpers.js
+++ jmvc/dependencies/helpers.js
@@ -1,39 +1,37 @@
-if(typeof cssQuery != 'undefined')
-	$$ = cssQuery
+if(typeof cssQuery != 'undefined') $$ = cssQuery;
 
 
 Object.extend = function(destination, source) {
-  for (var property in source) {
+  for (var property in source) 
     destination[property] = source[property];
-  }
   return destination;
 };
 String.prototype.capitalize = function() {
-    return this.slice(0,1).toUpperCase()+this.slice(1)
-}
+    return this.slice(0,1).toUpperCase()+this.slice(1);
+};
 String.prototype.uncapitalize = function() {
-    return this.slice(0,1).toLowerCase()+this.slice(1)
-}
+    return this.slice(0,1).toLowerCase()+this.slice(1);
+};
 String.prototype.include = function(pattern){
 	return this.indexOf(pattern) > -1;
-}
+};
 //chomp function for strings
 String.prototype.chomp = function(str){
-    var index = this.lastIndexOf(str)
-    return (index != -1 ? this.slice(0, index): this)
-}
+    var index = this.lastIndexOf(str);
+    return (index != -1 ? this.slice(0, index): this);
+};
 String.prototype.first_capitalize = function() {
     return this.substring(0,1).capitalize()+this.substring(1);
-}
+};
 String.prototype.clean = function(){
-    return this.replace(/[_!@\#\$\%^&*()\<\>\"\'\\?,.\{\}\[\]~\- ]/g, '').uncapitalize()
-}
+    return this.replace(/[_!@\#\$\%^&*()\<\>\"\'\\?,.\{\}\[\]~\- ]/g, '').uncapitalize();
+};
 
 Date.month_names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     
 Date.prototype.month_name = function() {
 	return Date.month_names[this.getMonth()-1];
-}
+};
 Date.prototype.number_of_days_in_month = function() {
     var year = this.getFullYear();
     var month = this.getMonth();
@@ -42,13 +40,13 @@ Date.prototype.number_of_days_in_month = function() {
     if (year%4 != 0) return m[1];
     if (year%100 == 0 && year%400 != 0) return m[1];
     return m[1] + 1;
-}
+};
 String.prototype.camalize = function(){
-	var parts = this.split('_')
+	var parts = this.split('_');
 	for(var i = 0; i < parts.length; i++){
 		parts[i] = parts[i].capitalize();
 	}
-	return parts.join('')
+	return parts.join('');
 };
 
 /* Cross-Browser Split v0.1; MIT-style license
@@ -105,4 +103,4 @@ String.prototype.split = function (s /* separator */, limit) {
 		(s.test("") ? output : output.concat("")) :
 		(limit      ? output : output.concat(this.slice(lastLastIndex)));
 };
-})()
+})();
