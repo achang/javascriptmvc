2c7e03a7d916797476ea606388b1274d5991a89a
brian.moschel <brian.moschel@f352ecdf-183f-0410-ae60-f1561b901441>
2008-02-03 02:08:07 +0000

    renamed lib to plugins
    git-svn-id: http://javascriptmvc.googlecode.com/svn/trunk@85 f352ecdf-183f-0410-ae60-f1561b901441

--- jmvc/jmvc.js
+++ jmvc/jmvc.js
@@ -56,7 +56,7 @@ JMVC.check_dependency = function(dependency_class_name, dependent_file_name) {
 include_plugin = function(plugin_name) {
 	var current_path = include.get_path();
 	include.set_path(JMVC_ROOT);
-	include(jFile.join('lib', plugin_name, 'setup'));
+	include(jFile.join('plugins', plugin_name, 'setup'));
 	include.set_path(current_path);
 }
 
