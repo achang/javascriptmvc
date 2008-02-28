a5fbfc2ff7784b656ee1349ec2426f4b7842751f
JustinBMeyer <JustinBMeyer@f352ecdf-183f-0410-ae60-f1561b901441>
2008-02-28 02:46:46 +0000

    better include
    git-svn-id: http://javascriptmvc.googlecode.com/svn/trunk@181 f352ecdf-183f-0410-ae60-f1561b901441

--- jmvc/include.js
+++ jmvc/include.js
@@ -340,47 +340,47 @@
 	//	include(INCLUDE_ROOT+'jmvc')
 	//}
 	
-	//JMVC stuff
-	JMVC = function() {};
-	JMVC.OPTIONS = {};
-	JMVC.Test = {};
+	//$MVC stuff
+	$MVC = function() {};
+	$MVC.OPTIONS = {};
+	$MVC.Test = {};
 	
 	//sets default app root to whatever page we are currently loading
 	//can be changed by set application root
 	(function() {
 		var jmvc_root = INCLUDE_ROOT, 
 			application_root = location.href.substring(0, location.href.lastIndexOf('/') )+ '/';
-		JMVC.root = function(){
+		$MVC.root = function(){
 			return jmvc_root;
 		};
-		JMVC.set_application_root = function(path){
+		$MVC.set_application_root = function(path){
 			application_root = path;
 		};
-		JMVC.get_application_root = function(){
+		$MVC.get_application_root = function(){
 			return application_root;
 		};
 	})();
 	
 	
 	/**
-	 * <p>Loads the correct version of JMVC.</p>
-	 * <p>Saves the user defined app_init_func to be executed later (once JMVC files are included).</p>
+	 * <p>Loads the correct version of $MVC.</p>
+	 * <p>Saves the user defined app_init_func to be executed later (once $MVC files are included).</p>
 	 */
-	JMVC.Initializer = function(user_initialize_function) {
+	$MVC.Initializer = function(user_initialize_function) {
 		if(include.get_path().lastIndexOf('/') == -1) {
-			JMVC.set_application_root('');
+			$MVC.set_application_root('');
 		} else {
 			var config_folder = include.get_path();
-			JMVC.set_application_root(include.get_path().substring(0, config_folder.lastIndexOf('/')));
+			$MVC.set_application_root(include.get_path().substring(0, config_folder.lastIndexOf('/')));
 		}
-		JMVC.user_initialize_function = user_initialize_function;
-		include(JMVC.root()+'/framework');
+		$MVC.user_initialize_function = user_initialize_function;
+		include($MVC.root()+'/framework');
 	};
 	
 	
 	include.plugin = function(plugin_name) {
 		var current_path = include.get_path();
-		include.set_path(JMVC.root());
+		include.set_path($MVC.root());
 		include('plugins/'+ plugin_name+'/setup');
 		include.set_path(current_path);
 	};
