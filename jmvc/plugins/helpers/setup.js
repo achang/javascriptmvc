0f62c0ded015732e3c4cffcf9321e11223b7655d
JustinBMeyer <JustinBMeyer@f352ecdf-183f-0410-ae60-f1561b901441>
2008-03-10 08:21:09 +0000

    packable helpers
    git-svn-id: http://javascriptmvc.googlecode.com/svn/trunk@215 f352ecdf-183f-0410-ae60-f1561b901441

--- jmvc/plugins/helpers/setup.js
+++ jmvc/plugins/helpers/setup.js
@@ -1,7 +1,9 @@
 
-if(typeof Prototype != 'undefined')
-	include({path: 'prototype_helpers.js', shrink_variables: false})
-else if(typeof jQuery != 'undefined')
-	include({path: 'jquery_helpers.js', shrink_variables: false})
-else
-	include({path: 'standard_helpers.js', shrink_variables: false})
+if(typeof Prototype != 'undefined'){
+	include({path: 'prototype_helpers.js', shrink_variables: false});
+}else if(typeof jQuery != 'undefined'){
+	include({path: 'jquery_helpers.js', shrink_variables: false});
+}else{
+	include({path: 'standard_helpers.js', shrink_variables: false});
+}
+	
