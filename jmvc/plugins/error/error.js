afd803d8761bd45a9b2062c12015dee70ae83258
JustinBMeyer <JustinBMeyer@f352ecdf-183f-0410-ae60-f1561b901441>
2008-03-14 23:14:25 +0000

    shortenned and correct background location
    git-svn-id: http://javascriptmvc.googlecode.com/svn/trunk@237 f352ecdf-183f-0410-ae60-f1561b901441

--- jmvc/plugins/error/error.js
+++ jmvc/plugins/error/error.js
@@ -42,8 +42,8 @@ $MVC.Object.extend(ApplicationError,{
 	},
 	create_title: function(){
 		var title = document.createElement('div');
-		title.style.background = 'url('+$MVC.root+'plugins/error/background.png) repeat-x scroll center top;';
-		title.style.backgroundImage = 'url('+$MVC.root+'plugins/error/background.png)';
+		//title.style.background = 'url('+$MVC.root+'/plugins/error/background.png) repeat-x scroll center top;';
+		title.style.backgroundImage = 'url('+$MVC.root+'/plugins/error/background.png)';
 		title.style.backgroundAttachment = 'scroll';
 		title.style.backgroundRepeat = 'repeat-x';
 		title.style.backgroundPosition = 'center top';
@@ -61,6 +61,7 @@ $MVC.Object.extend(ApplicationError,{
 		form.onsubmit = callback;
 		form.innerHTML ="<div style='float: left; width: 300px;margin-left:"+leftmargin+"px;'>Something just went wrong.  Please describe your most recent actions and let us know what happenned. We'll fix the problem.</div>"+
 		    "<input type='submit' value='Send' style='font-size: 12pt; float:right; margin: 17px 5px 0px 0px; width:60px;padding:5px;'/>"+
+			"<textarea style='width: 335px; color: gray;' rows='"+($MVC.Browser.Gecko ? 2 : 3)+"' name='description' id='_error_text' "+
 			"<textarea style='width: 335px; color: gray;' rows='"+($MVC.Browser.IE || $MVC.Browser.Opera || $MVC.Browser.WebKit ? 3 : 2)+"' name='description' id='_error_text' "+
 			"onfocus='ApplicationError.text_area_focus();' "+
 			"onblur='ApplicationError.text_area_blur();' >"+this.textarea_text+"</textarea>";
