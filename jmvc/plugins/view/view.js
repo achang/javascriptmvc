4852c4828f0870b4aec105e10c20967603e5a0b3
brian.moschel <brian.moschel@f352ecdf-183f-0410-ae60-f1561b901441>
2008-04-03 19:33:48 +0000

    fixed view include
    git-svn-id: http://javascriptmvc.googlecode.com/svn/trunk@412 f352ecdf-183f-0410-ae60-f1561b901441

--- jmvc/plugins/view/view.js
+++ jmvc/plugins/view/view.js
@@ -437,7 +437,7 @@ include.view = function(path){
 		new $MVC.View({url: path});
 	}else if(include.get_env() == 'compress'){
 		var oldp = include.get_path();
-		include.set_path($MVC.root.path);
+		include.set_path($MVC.root.join('views'));
 		include({path: path, process: $MVC.View.process_include, ignore: true});
 		include.set_path(oldp);
 		new $MVC.View({url: path});
@@ -448,7 +448,7 @@ include.view = function(path){
 
 include.views = function(){
 	for(var i=0; i< arguments.length; i++){
-		include.view('views/'+arguments[i]+'.ejs');
+		include.view(arguments[i]+'.ejs');
 	}
 };
 
