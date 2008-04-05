fb47cc2125f80f4e4481202df783ee2ac4a9f11c
JustinBMeyer <JustinBMeyer@f352ecdf-183f-0410-ae60-f1561b901441>
2008-04-05 00:08:32 +0000

    inflector test
    git-svn-id: http://javascriptmvc.googlecode.com/svn/trunk@421 f352ecdf-183f-0410-ae60-f1561b901441

--- apps/helpers_test.js
+++ apps/helpers_test.js
@@ -1,3 +1,3 @@
 include.plugins('ajax','controller','view');
-include.unit_tests('helpers', 'ajax', 'view','controller_view', 'element');
+include.unit_tests('helpers', 'ajax', 'view','controller_view', 'element','inflector');
 include.controllers('controllertest');
---
+++ test/unit/inflector_test.js
@@ -0,0 +1,18 @@
+new $MVC.Test.Unit('inflector',{
+	test_singularize : function(){
+		this.assert_equal('person', $MVC.String.singularize('people'));
+		this.assert_equal('dog', $MVC.String.singularize('dogs'));
+	},
+	test_pluralize : function(){
+		this.assert_equal('people', $MVC.String.pluralize('person'));
+		this.assert_equal('dogs', $MVC.String.pluralize('dog'));
+	},
+	test_conflict_singular : function(){
+		this.assert_equal('person', 'people'.singularize());
+		this.assert_equal('dog', 'dogs'.singularize());
+	},
+	test_conflict_pluralize : function(){
+		this.assert_equal('people', 'person'.pluralize(), "OK if no conflict");
+		this.assert_equal('dogs', 'dog'.pluralize(), "OK if no conflict");
+	}
+});
