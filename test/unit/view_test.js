a0f00478acf85ae44246e3e36949527eb0727f4d
JustinBMeyer <JustinBMeyer@f352ecdf-183f-0410-ae60-f1561b901441>
2008-05-12 20:57:51 +0000

    merging changes from 1.0 view testing
    git-svn-id: http://javascriptmvc.googlecode.com/svn/trunk@477 f352ecdf-183f-0410-ae60-f1561b901441

--- test/unit/view_test.js
+++ test/unit/view_test.js
@@ -4,5 +4,9 @@ new MVC.Test.Unit('view',{
 	},
 	test_render : function(){
 		this.assert_equal("<h1>yes</h1>\n\n<p>1</p>\n\n<p>2</p>\n\n<p>3</p>\n", new MVC.View({url: 'no_helpers'}).render( {data: {title: 'yes', info: [1,2,3]}}) );
-	}
+	},
+    test_nested : function(){
+        secondTemplate = new MVC.View({url: 'second'});
+        this.assert_equal('<div><p>1</p>2</div>', new MVC.View({url: 'first'}).render({x: 1, y: 2})  )
+    }
 })
