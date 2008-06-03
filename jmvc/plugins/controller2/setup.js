3f7a98e3dd1e24d7d945511672e9feb4ff51c56d
brian.moschel <brian.moschel@f352ecdf-183f-0410-ae60-f1561b901441>
2008-06-03 23:12:51 +0000

    replacing controller with controller2
    git-svn-id: http://javascriptmvc.googlecode.com/svn/trunk@529 f352ecdf-183f-0410-ae60-f1561b901441

--- jmvc/plugins/controller2/setup.js
+++
@@ -1,4 +0,0 @@
-include.plugins('helpers','inflector','event','class2');
-include('delegator','controller');
-if(MVC.View) include.plugins('controller_view');
-if(include.get_env() == 'test') include('test')
