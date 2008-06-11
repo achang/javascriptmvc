7d351c2ad04f0fc5f994a39dff8c7e0336b4a374
brian.moschel <brian.moschel@f352ecdf-183f-0410-ae60-f1561b901441>
2008-06-11 09:07:59 +0000

    added plugin descriptions
    git-svn-id: http://javascriptmvc.googlecode.com/svn/trunk@592 f352ecdf-183f-0410-ae60-f1561b901441

---
+++ jmvc/command/resources/plugin_descriptions.js
@@ -0,0 +1,98 @@
+MVC.PluginDescriptions = {
+	'ajax': {
+		url: 'http://javascriptmvc.com/learningcenter/ajax/index.html', 
+		description: 'Update content without refreshing the page'
+	},
+	'class': {
+		url: 'http://javascriptmvc.com/learningcenter/class/index.html', 
+		description: 'Lightweight Ruby-like class system'
+	},
+	'controller': {
+		url: 'http://javascriptmvc.com/learningcenter/controller/index.html', 
+		description: 'Respond to events'
+	},
+	'controller_view': {
+		url: 'http://javascriptmvc.com/learningcenter/controller/api.html#Controller.render', 
+		description: 'Ties View and Controller together with render'
+	},
+	'core': {
+		url: 'http://javascriptmvc.com/learningcenter/core/index.html', 
+		description: 'A plugin that loads a bunch of commonly useful plugins'
+	},
+	'date': {
+		url: 'http://javascriptmvc.com/learningcenter/date/index.html', 
+		description: 'Helpers for working with dates'
+	},
+	'debug': {
+		url: 'http://javascriptmvc.com/learningcenter/debug/index.html', 
+		description: 'Log debug messages to a console'
+	},
+	'dragdrop': {
+		url: 'http://javascriptmvc.com/blog/?p=59', 
+		description: 'Simple drag and drop with controller actions'
+	},
+	'element': {
+		url: 'http://javascriptmvc.com/learningcenter/element/index.html', 
+		description: 'Manipulate and traverse the DOM'
+	},
+	'error': {
+		url: 'http://javascriptmvc.com/learningcenter/error/index.html', 
+		description: 'Receive email notification when users encounter an error'
+	},
+	'event': {
+		url: 'http://javascriptmvc.com/learningcenter/event/index.html', 
+		description: 'Register events without worrying about memory issues'
+	},
+	'helpers': {
+		url: 'http://javascriptmvc.com/learningcenter/helpers/index.html', 
+		description: 'String, Array, Function and Object extensions'
+	},
+	'history': {
+		url: 'http://javascriptmvc.com/learningcenter/history/index.html', 
+		description: 'Provide back, forward, and refresh button functionality'
+	},
+	'inflector': {
+		url: 'http://javascriptmvc.com/learningcenter/inflector/index.html', 
+		description: 'Pluralizing and singularizing string helpers'
+	},
+	'model': {
+		url: 'http://javascriptmvc.com/learningcenter/model/index.html', 
+		description: 'Connect to a server data source'
+	},
+	'model_ajax': {
+		url: 'http://javascriptmvc.com/learningcenter/model/index.html', 
+		description: 'Connect to a data source on your own domain'
+	},
+	'model_cookie': {
+		url: 'http://javascriptmvc.com/learningcenter/model/index.html', 
+		description: 'Connect to cookies in your browser'
+	},
+	'model_remote': {
+		url: 'http://javascriptmvc.com/learningcenter/model/index.html', 
+		description: 'Connect to cross-domain data sources'
+	},
+	'query': {
+		url: 'http://javascriptmvc.com/learningcenter/query/index.html', 
+		description: 'CSS DOM element selection'
+	},
+	'remote': {
+		url: 'http://javascriptmvc.com/learningcenter/remote/index.html', 
+		description: 'Connect to cross-domain data sources'
+	},
+	'test': {
+		url: 'http://javascriptmvc.com/learningcenter/test/index.html', 
+		description: 'Test your JavaScript functionality'
+	},
+	'test': {
+		url: 'http://javascriptmvc.com/learningcenter/test/index.html', 
+		description: 'Test your JavaScript functionality'
+	},
+	'view': {
+		url: 'http://javascriptmvc.com/learningcenter/view/index.html', 
+		description: 'Clean HTML out of your JavaScript with templates'
+	},
+	'view_helpers': {
+		url: 'http://javascriptmvc.com/learningcenter/view_helpers/index.html', 
+		description: 'Simplify your templates with shortcuts'
+	}
+}
